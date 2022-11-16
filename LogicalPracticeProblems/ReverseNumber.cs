82032b39665a054398a3f80baf4cd02a5d3ddfe7
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-16 16:21:59 +0530

    Added ReverseNumber Problem In Logical Programming

---
+++ LogicalPracticeProblems/ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPracticeProblems
+{
+    public static class ReverseNumber
+    {
+        public static void ReversedNumber()
+        {
+            Console.WriteLine("enter a Number");
+            //Variables
+            int num = int.Parse(Console.ReadLine());
+            int rev = 0;
+            //WhileLoop For Iteration
+            while (num > 0)
+            {
+                //Condition for Reversing Number
+                int rem = num % 10;
+                rev = (rev * 10) + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("{0} is the Reversed Number",rev);
+        }
+    }
+}

3fe27ff162c0e70e5222a6339272e1f674d36220
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-16 16:12:22 +0530

    Added PrimeNumber Problem In Logical Programming

---
+++ LogicalPracticeProblems/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPracticeProblems
+{
+    public static class PrimeNumber
+    {
+        public static void NumberCheck()
+        {
+            Console.Write("Enter a Number : ");
+            //Variable
+            int number = int.Parse(Console.ReadLine());
+            int IsPrime = 0;
+            //Itreating Using For Loop
+            for (int i = 2; i < number; i++)
+            {
+                //Condition For Checking Number Is prime or Not Prime
+                if (number % i == 0)
+                {
+                    Console.Write("{0} is not a Prime Number.",number);
+                    IsPrime = 1;
+                    break;
+                }
+            }
+            if (IsPrime == 0)
+            {
+                Console.Write("{0} is a Prime Number.",number);
+            }
+        }
+    }
+}
